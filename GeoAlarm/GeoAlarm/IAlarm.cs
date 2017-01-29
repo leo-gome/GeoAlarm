3d7ed0845ea8c253c827f2c8f6e74e3d9b56e582
DragonScarlet <dimknight@gmail.com>
2017-01-29 17:03:35 +0100

    Interface Added

---
+++ GeoAlarm/GeoAlarm/IAlarm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeoAlarm
+{
+    interface IAlarm
+    {
+        string name
+        {
+            get;
+            set;
+        }
+
+        //ToDo: Get alternative to string
+        string name
+        {
+            get;
+            set;
+        }
+
+        float areaSize
+        {
+            get;
+            set;
+        }
+
+        //ToDo: Get alternative to string
+        string[] timeInterval
+        {
+            get;
+            set;
+        }
+
+        bool[] weekDays
+        {
+            get;
+            set;
+        }
+    }
+}
