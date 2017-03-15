92fd5ef73629e49daf8109eef3f8ed351ff3fd82
DragonScarlet <dimknight@gmail.com>
2017-03-15 22:17:36 +0100

    Alarm.cs added

---
+++ GeoAlarm/GeoAlarm/Alarm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace GeoAlarm
+{
+    public class Alarm
+    {
+        public string Name { get; set; }
+        public TimePicker StartTime { get; set; }
+        public TimePicker EndTime { get; set; }
+        public double Radius { get; set; }
+    }
+}
